6a88cc0d3bbcaa0150c41c81c7594901d9a98f2a
pee37100 <591559@egat.co.th>
2026-02-05 16:40:07 +0700

    first commit

--- app/(front)/blog/page.tsx
+++ app/(front)/blog/page.tsx
@@ -3,9 +3,6 @@ export default async function BlogPage() {
     // 2. จำลองการโหลดข้อมูล (หน่วงเวลา 3 วินาที)
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    // 3. จำลอง Error
-    throw new Error("ไม่สามารถโหลดบทความได้");
-
     return (
       <div className="p-8">
         <h1 className="text-3xl font-bold">บทความ</h1>
