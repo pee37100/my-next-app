f21ee6465e0d14bd4ed4d2935806c3201f9c06f3
pee37100 <591559@egat.co.th>
2026-02-06 10:21:36 +0700

    second

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,46 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
-export default function page() {
+
+
+
+export default function LoginPage() {
+  const router = useRouter()  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState(''); 
+  
+//function to handle login and check credentials 
+ const authenticateUser = (e: React.FormEvent) => {
+  e.preventDefault()
+    if (username === 'admin' && password === 'password') {
+      alert('Login successful')
+      router.push('/counter');}
+      else {
+      alert('Invalid credentials')
+    } 
+  };
+
+
   return (
     <div className='flex flex-col justify-center'><h2 className="text-2xl font-bold">Login Page</h2>
-  <form className="max-w-md mx-auto mt-8">
+  <form onSubmit={authenticateUser} className="max-w-md mx-auto mt-8">
     <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username"> Username </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+      <input 
+      onChange={(e) => setUsername(e.target.value)}
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
     </div>
     <div className="mb-6">  
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password"> Password </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+      <input 
+      onChange={(e) => setPassword(e.target.value)}
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
     </div>
     <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"> Sign In </button>
+      <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit"> Sign In </button>
     </div>
   </form>
   <p className="text-center mt-4"><Link href="/" className="text-blue-500 hover:text-blue-700">Back to Home</Link></p>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,23 @@
-import Link from "next/link"
-export default function Navbar() {
-  return (
-    <header className="flex bg-blue-800 text-amber-100 font-bold text-2xl p-4">
-      <h1 className="text-3xl font-bold"><Link href="/" className="hover:text-blue-600">
-        My app
-      </Link></h1>
- <ul className="flex gap-4 p-4 text-xl">
-      <Link href="/" className="hover:text-blue-600">
-        หน้าแรก
-      </Link>
-      <Link href="/about" className="hover:text-blue-600">
-        เกี่ยวกับเรา
-      </Link>
-      <Link href="/blog" className="hover:text-blue-600">
-        บทความ
-      </Link>
-      <Link href="/contact" className="hover:text-blue-600">
-        ติดต่อ
-      </Link>
-            <Link href="/counter" className="hover:text-blue-600">
-        Counter
-      </Link>
-            <Link href="/login" className="hover:text-blue-600">
-        login
-      </Link>
-    </ul>
-    </header>
+"use client";
  
+import Link from "next/link";
+import { usePathname } from "next/navigation";
  
- 
-  )
+export default function Navbar ( )
+{
+     const pathname = usePathname ( );
+     console.log ( pathname );
+     return (
+          <header className="bg-blue-600 p-4 text-white flex">
+               <h1 className="text-3xl font-bold">Navbar</h1>
+               <ul className="flex space-x-4 ml-8 mt-2">
+                    <li><Link className={ pathname === '/' ? 'text-amber-400' : ''} href="/">Home</Link></li>
+                    <li><Link className={ pathname === '/about' ? 'text-amber-400' : ''} href="/about">About</Link></li>
+                    <li><Link className={ pathname === '/contact' ? 'text-amber-400' : ''} href="/contact">Contact</Link></li>
+                    <li><Link className={ pathname === '/blog' ? 'text-amber-400' : ''} href="/blog">Blog</Link></li>
+                    <li><Link className={ pathname === '/counter' ? 'text-amber-400' : ''} href="/counter">Counter</Link></li>
+                    <li><Link className={ pathname === '/login' ? 'text-amber-400' : ''} href="/login">Login</Link></li>
+               </ul>
+          </header>
+     )
 }
